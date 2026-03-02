dedeacffe213f01ca31d7f13fbd831619f20884f
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-02 14:40:15 -0500

    added

--- handling.py
+++ handling.py
@@ -8,4 +8,7 @@ with open('example.txt','r') as file:
 with open('example.txt','w') as file:
     file.write('hello world:\n')
     file.write('i am good')
+with open('example.txt','a') as file:
+    file.write('hi,everyone')
+
     
